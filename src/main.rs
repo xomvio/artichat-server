acae01d871bd2af025c04c85e8bf93f59b271c67
xomvio <xomvio@proton.me>
2025-01-13 21:04:11 +0300

    sending message each other now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::net::{TcpListener, UdpSocket};
+use std::collections::HashMap;
+use std::net::{SocketAddr, TcpListener, UdpSocket};
 use std::io::Read;
 fn main() {
 
     let socket = UdpSocket::bind("127.0.0.1:9595").unwrap();
 
+    let mut rooms: HashMap<String, Vec<SocketAddr>> = HashMap::new();
     loop {
         let mut buffer = [0; 1024];
         match socket.recv_from(&mut buffer) {
             Ok((size, addr)) => {
                 println!("Request: {}", String::from_utf8_lossy(&buffer));
-                socket.send_to(&buffer[..size], addr).unwrap();
+                let room: String = String::from_utf8_lossy(&buffer[..32]).to_string();
+                match rooms.get_mut(&room) {
+                    Some(users) => {
+                        if !users.contains(&addr) {
+                            users.push(addr);
+                        }
+                        for user in users.iter() {
+                            socket.send_to(&buffer[32..size], *user).unwrap();
+                        }
+                    },
+                    None => {
+                        rooms.insert(room.clone(), vec![addr]);
+                    }
+                }
+                //socket.send_to(&buffer[32..size], addr).unwrap();
             },
             Err(e) => println!("Failed to read from connection: {}", e),
         }
