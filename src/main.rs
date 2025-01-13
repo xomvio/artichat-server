f6aa44cc3df4740e26f0b063f23a7fb774ff8f7a
xomvio <xomvio@proton.me>
2025-01-14 00:03:39 +0300

    last commit before torify

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
 use std::collections::HashMap;
 use std::net::{SocketAddr, TcpListener, UdpSocket};
 use std::io::Read;
+
+struct User {
+    pub name: String,
+    pub addr: SocketAddr
+}
 fn main() {
 
     let socket = UdpSocket::bind("127.0.0.1:9595").unwrap();
 
-    let mut rooms: HashMap<String, Vec<SocketAddr>> = HashMap::new();
+    let mut rooms: HashMap<String, Vec<User>> = HashMap::new();
     loop {
         let mut buffer = [0; 1024];
         match socket.recv_from(&mut buffer) {
             Ok((size, addr)) => {
                 println!("Request: {}", String::from_utf8_lossy(&buffer));
                 let room: String = String::from_utf8_lossy(&buffer[..32]).to_string();
-                match rooms.get_mut(&room) {
-                    Some(users) => {
-                        if !users.contains(&addr) {
-                            users.push(addr);
+                match rooms.get_mut(&room) {// is there a room with this id
+                    Some(users) => {// there is
+                        // check if the user is already in the room
+                        if users.iter().find(|user| user.addr == addr).is_none() {// user is not in the room yet
+                            // add him to the room
+                            users.push(User { name: String::from_utf8_lossy(&buffer[32..size]).to_string(), addr });
+                            // send all usernames in the room to the new user
+                            for user in users.iter() {
+                                if user.addr == addr {
+                                    continue;
+                                }
+                                // send message
+                                socket.send_to(user.name.as_bytes(), addr).unwrap();
+                            }
                         }
-                        for user in users.iter() {
-                            socket.send_to(&buffer[32..size], *user).unwrap();
+                        
+                        // send message to all users in the room
+                        for user in users.iter() { // for each user
+                            // send message
+                            socket.send_to(&buffer[32..size], user.addr).unwrap();
                         }
                     },
-                    None => {
-                        rooms.insert(room.clone(), vec![addr]);
+                    None => {// there isn't
+                        // create a new room
+                        rooms.insert(room.clone(), vec![User { name: String::from_utf8_lossy(&buffer[32..size]).to_string(), addr }]);
+                        // only send his own name to room creator because he's the only one in the room. lonely.
+                        socket.send_to(&buffer[32..size], addr).unwrap();
                     }
                 }
-                //socket.send_to(&buffer[32..size], addr).unwrap();
             },
             Err(e) => println!("Failed to read from connection: {}", e),
         }
